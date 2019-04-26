3fca36403fda4bc5dc73ba07659c2a41e5ab6e55
Jose72 <jjjtony_72@hotmail.com>
2019-04-26 18:34:36 -0300

    minor test stuff

--- test/curl_gets.sh
+++ test/curl_gets.sh
@@ -1,5 +1,3 @@
-
-curl -v -X POST -H "Content-Type: application/json" -H 'Expect:' --data @"./post.json" http://127.0.0.1:6060/log
-
 curl -v -X GET -H "Content-Type: application/json" -H 'Expect:' --data @"./query_1.json" http://127.0.0.1:6070/log
+curl -v -X GET -H "Content-Type: application/json" -H 'Expect:' --data @"./query_2.json" http://127.0.0.1:6070/log
 
